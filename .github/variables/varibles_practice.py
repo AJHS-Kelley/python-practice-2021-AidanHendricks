a939bf109e835321b9c11c94df75573784d91749
AidanHendricks <90405653+AidanHendricks@users.noreply.github.com>
2021-10-28 12:05:38 -0400

    (v0.1) Variable Practice
    Added and printed variables.

--- .github/variables/varibles_practice.py
+++
@@ -1,1 +0,0 @@
-# Variable Pracitce, Aidan Hendricks, 10/28/21 11:40pm, version 0.0
---
+++ variables/varibles_practice.py
@@ -0,0 +1,11 @@
+# Variable Pracitce, Aidan Hendricks, 10/28/21 11:49pm, version 0.1
+
+# Variable Types -- Most Common
+
+my_float = 0.87
+my_int = 2
+my_string = "bruh moment"
+
+print(my_float)
+print(my_int)
+print(my_string)
